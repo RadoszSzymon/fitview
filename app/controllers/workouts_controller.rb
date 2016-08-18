bb98c3e15118a1b9e8d4c1f17de319fcf187084d
RadoszSzymon <radoszszymon@gmail.com>
2016-08-18 17:57:12 +0000

    Added new trainings functionality and display logs in index page

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,4 +1,43 @@
 class WorkoutsController < ApplicationController
+    before_action :find_training, only: [:show, :edit, :update, :destroy]
+    
+    
     def index
+        @trainings = Workout.all.order("created_at DESC")
+    end
+    
+    def show
+    end
+    
+    def new 
+        @training = Workout.new
+    end
+    
+    def create 
+         @training = Workout.new(workout_params)
+         if @training.save
+             redirect_to @training
+         else
+             render 'new'
+         end
+    end
+    
+    def edit 
+    end
+    
+    def update
+    end
+    
+    def destroy
+    end
+    
+    private
+    
+    def workout_params
+        params.require(:workout).permit(:date, :workout, :mood, :length)
+    end
+    
+    def find_training
+        @training = Workout.find(params[:id])
     end
 end
