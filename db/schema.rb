09dc82244ba3ccdf685804c24119b9f6623bf61f
RadoszSzymon <radoszszymon@gmail.com>
2016-08-21 15:11:27 +0000

    displayed different menu options for log in users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160821084828) do
+ActiveRecord::Schema.define(version: 20160821145403) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 20160821084828) do
     t.integer  "weight"
     t.integer  "age"
     t.string   "sex"
+    t.string   "name"
+    t.string   "surname"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
