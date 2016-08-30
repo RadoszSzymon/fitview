7f7a3af0678c24fe2159a1b8984f8146f8ecac0d
RadoszSzymon <radoszszymon@gmail.com>
2016-08-30 09:44:59 +0200

    added basic information about user

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -1,4 +1,17 @@
 class InfoController < ApplicationController
     def about
+        @bmi = (current_user.weight.to_f / ((current_user.height.to_f / 100) * (current_user.height.to_f / 100))).round(2)
+        
+        @full_distance = current_user.workouts.inject(0) { |total, training| total + training.distance } 
+        
+        @full_time = current_user.workouts.inject(0) { |total, training| total + training.length }
+        
+        #@full_calories = current_user.workouts.inject(0) { |total, training| total + @calories }
+        
+        if current_user.sex == "Male"
+            @bmr = (current_user.weight * 10) + (current_user.height * 6.25) - (current_user.age - 5) + 5
+        elsif current_user.sex == "Female"
+            @bmr = (current_user.weight * 10) + (current_user.height * 6.25) - (current_user.age - 5) - 161
+        end
     end
 end
