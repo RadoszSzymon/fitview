f654718c54dc633c3f5b53eaa0e6aa3b290a9d81
RadoszSzymon <radoszszymon@gmail.com>
2016-08-30 22:23:43 +0200

    added user avatar

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,10 +3,10 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:name, :goals, :surname, :email, :password, :password_confirmation, :weight, :activity, :height, :age, :sex)
+    params.require(:user).permit(:name, :avatar, :goals, :surname, :email, :password, :password_confirmation, :weight, :activity, :height, :age, :sex)
   end
 
   def account_update_params
-    params.require(:user).permit(:name, :goals,  :surname, :email, :password, :password_confirmation, :activity, :current_password, :weight, :height, :age, :sex)
+    params.require(:user).permit(:name, :avatar, :goals,  :surname, :email, :password, :password_confirmation, :activity, :current_password, :weight, :height, :age, :sex)
   end
 end
