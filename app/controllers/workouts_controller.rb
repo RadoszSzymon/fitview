d1f06fad46162ad8845d80a15be127089e325ab0
RadoszSzymon <radoszszymon@gmail.com>
2016-08-31 21:44:14 +0200

    current status before goals list

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -3,37 +3,37 @@ class WorkoutsController < ApplicationController
     before_action :authenticate_user!, except: [:index, :show]
     
     def index
-        @trainings = Workout.all.order("created_at DESC")
+        @workouts = Workout.all
     end
     
     def show
         if current_user.sex == "Male"
-            if @training.intensity == "High"
-                @calories = (((current_user.age * 0.2017) - (current_user.weight * 0.09036) + (165 * 0.6309) - 55.0969) * @training.length / 4.184).round(2)
-            elsif @training.intensity == "Normal"
-                @calories = (((current_user.age * 0.2017) - (current_user.weight * 0.09036) + (145 * 0.6309) - 55.0969) * @training.length / 4.184).round(2)
-            elsif @training.intensity == "Low"
-                @calories = (((current_user.age * 0.2017) - (current_user.weight * 0.09036) + (125 * 0.6309) - 55.0969) * @training.length / 4.184).round(2) 
+            if @workout.intensity == "High"
+                @calories = (((current_user.age * 0.2017) - (current_user.weight * 0.09036) + (165 * 0.6309) - 55.0969) * @workout.length / 4.184).round(2)
+            elsif @workout.intensity == "Normal"
+                @calories = (((current_user.age * 0.2017) - (current_user.weight * 0.09036) + (145 * 0.6309) - 55.0969) * @workout.length / 4.184).round(2)
+            elsif @workout.intensity == "Low"
+                @calories = (((current_user.age * 0.2017) - (current_user.weight * 0.09036) + (125 * 0.6309) - 55.0969) * @workout.length / 4.184).round(2) 
             end
         else
-            if @training.intensity == "High"
-                @calories = (((current_user.age * 0.074) - (current_user.weight * 0.05741) + (165 * 0.4472) - 20.4022) * @training.length / 4.184).round(2)
-            elsif @training.intensity == "Normal"
-                @calories = (((current_user.age * 0.074) - (current_user.weight * 0.05741) + (145 * 0.4472) - 20.4022) * @training.length / 4.184).round(2)
-            elsif @training.intensity == "Low"
-                @calories = (((current_user.age * 0.074) - (current_user.weight * 0.05741) + (125 * 0.4472) - 20.4022) * @training.length / 4.184).round(2)
+            if @workout.intensity == "High"
+                @calories = (((current_user.age * 0.074) - (current_user.weight * 0.05741) + (165 * 0.4472) - 20.4022) * @workout.length / 4.184).round(2)
+            elsif @workout.intensity == "Normal"
+                @calories = (((current_user.age * 0.074) - (current_user.weight * 0.05741) + (145 * 0.4472) - 20.4022) * @workout.length / 4.184).round(2)
+            elsif @workout.intensity == "Low"
+                @calories = (((current_user.age * 0.074) - (current_user.weight * 0.05741) + (125 * 0.4472) - 20.4022) * @workout.length / 4.184).round(2)
             end
         end
     end
     
     def new 
-        @training = current_user.workouts.build
+        @workout = current_user.workouts.build
     end
     
     def create 
-         @training = current_user.workouts.build(workout_params)
-         if @training.save
-             redirect_to @training
+         @workout = current_user.workouts.build(workout_params)
+         if @workout.save
+             redirect_to @workout
          else
              render 'new'
          end
@@ -43,15 +43,15 @@ class WorkoutsController < ApplicationController
     end
     
     def update
-        if @training.update(workout_params)
-            redirect_to @training
+        if @workout.update(workout_params)
+            redirect_to @workout
         else
             render 'edit'
         end
     end
     
     def destroy
-        @training.destroy
+        @workout.destroy
         redirect_to root_path
     end
     
@@ -62,6 +62,6 @@ class WorkoutsController < ApplicationController
     end
     
     def find_training
-        @training = Workout.find(params[:id])
+        @workout = Workout.find(params[:id])
     end
 end
