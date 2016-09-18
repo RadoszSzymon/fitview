5c02c0c0aa866ae891acce522865b8f70cb9bb7b
RadoszSzymon <radoszszymon@gmail.com>
2016-09-18 10:42:30 +0200

    last 3 weeks training distance morris graph

--- app/helpers/workouts_helper.rb
+++ app/helpers/workouts_helper.rb
@@ -1,2 +1,10 @@
 module WorkoutsHelper
+	def sessions_chart_data
+ 		(3.weeks.ago.to_date..Date.today).map do |date|
+ 			{
+ 				date: date,
+ 				distance: Workout.where("date(date) = ?", date).sum(:distance)
+ 			}
+ 		end
+	end
 end
