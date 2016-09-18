548b983fba8b8293264e66b1c66de62e99c36977
RadoszSzymon <radoszszymon@gmail.com>
2016-09-18 17:17:25 +0200

    distance graphs for running, swimming and cycling

--- app/helpers/workouts_helper.rb
+++ app/helpers/workouts_helper.rb
@@ -7,4 +7,31 @@ module WorkoutsHelper
  			}
  		end
 	end
+
+	def running_chart_data
+ 		(3.weeks.ago.to_date..Date.today).map do |date|
+	    {
+	        date: date,
+	        distance: Workout.where(workout: "Running", date: date.beginning_of_day..date.end_of_day).sum(:distance)
+	    }
+	  end
+	end
+
+	def swimming_chart_data
+ 		(3.weeks.ago.to_date..Date.today).map do |date|
+	    {
+	        date: date,
+	        distance: Workout.where(workout: "Swimming", date: date.beginning_of_day..date.end_of_day).sum(:distance)
+	    }
+	  end
+	end
+
+	def cycling_chart_data
+ 		(3.weeks.ago.to_date..Date.today).map do |date|
+	    {
+	        date: date,
+	        distance: Workout.where(workout: "Cycling", date: date.beginning_of_day..date.end_of_day).sum(:distance)
+	    }
+	  end
+	end
 end
