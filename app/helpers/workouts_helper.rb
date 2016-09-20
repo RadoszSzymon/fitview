343309276fe7369ca2699baec6ed46a1f03c0fa1
RadoszSzymon <radoszszymon@gmail.com>
2016-09-20 16:46:20 +0200

    display all distances on one graph

--- app/helpers/workouts_helper.rb
+++ app/helpers/workouts_helper.rb
@@ -26,11 +26,13 @@ module WorkoutsHelper
 	  end
 	end
 
-	def cycling_chart_data
+	def distance_chart_data
  		(3.weeks.ago.to_date..Date.today).map do |date|
 	    {
 	        date: date,
-	        distance: Workout.where(workout: "Cycling", date: date.beginning_of_day..date.end_of_day).sum(:distance)
+	        distance_c: Workout.where(workout: "Cycling", date: date.beginning_of_day..date.end_of_day).sum(:distance),
+					distance_r: Workout.where(workout: "Running", date: date.beginning_of_day..date.end_of_day).sum(:distance),
+				  distance_s: Workout.where(workout: "Swimming", date: date.beginning_of_day..date.end_of_day).sum(:distance)
 	    }
 	  end
 	end
