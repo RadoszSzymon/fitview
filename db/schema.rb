bc4d23e91cb36bbe440baa61e5b15609eca158da
radoszszymon <radoszszymon@gmail.com>
2016-10-28 09:57:06 +0000

    added links in footer

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@
 ActiveRecord::Schema.define(version: 20160831082620) do
 
   create_table "users", force: :cascade do |t|
+    t.boolean  "admin"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
